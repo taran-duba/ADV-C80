f10bb1aa769d3ad62b0fbbb26c792b7bff1908cc
taran-duba <72531052+taran-duba@users.noreply.github.com>
2022-01-15 19:00:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-function join() {
-    //Left
-    var j1 = document.getElementById("j1").value;
-    var j2 = document.getElementById("j2").value;
-    var j3 = document.getElementById("j3").value;
-    var j4 = document.getElementById("j4").value;
-    var j5 = document.getElementById("j5").value;
-    var j6 = document.getElementById("j6").value;
-    var left_side = j1.concat(" ", j2," ", j3, " ", j4, " ", j5," ", j6);
-    //Right
-    var j01 = document.getElementById("j01").value;
-    var j02 = document.getElementById("j02").value;
-    var j03 = document.getElementById("j03").value;
-    var j04 = document.getElementById("j04").value;
-    var j05 = document.getElementById("j05").value;
-    var j06 = document.getElementById("j06").value;
-    var right_side = j01.concat(" ", j02," ", j03, " ", j04, " ", j05," ", j06);
-    var paragraph = left_side + " " + right_side;
-    document.getElementById("display-paragraphs").innerHTML = paragraph;
+function getParagraph1()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("div1_input_box_" + i).value);
+    }
+document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
+}
+
+function getParagraph2()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+        inputs.push(document.getElementById("div2_input_box_" + i).value);
+    }
+document.getElementById("showParagraph2").innerHTML = inputs.join(". ");
 }
